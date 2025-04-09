ac195d3144e5f31be921e8f1294dfe22906988d0
Suhail-dev1 <suhailansari3695@gmail.com>
2025-04-09 16:19:31 +0530

    Update synctvshows route and other changes

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, HTTPException
 from app.db import get_db_connection
 from app.routes.users import router as users_router
 from app.routes.sync import router as sync_router
+from app.routes.synctvshows import router as synctvshows_router 
 
 app = FastAPI()
 
 app.include_router(users_router)
 app.include_router(sync_router)
+app.include_router(synctvshows_router)
