9b75210635424c878203d05d1817b931928da2a3
vishal Maurya <vishal@aerohub.aero>
2025-04-14 11:56:45 +0530

    disable user menu

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from app.db import get_db_connection
-from app.routes.users import router as users_router
+# from app.routes.users import router as users_router
 from app.routes.sync import router as sync_router
 from app.routes.synctvshows import router as synctvshows_router 
 from app.routes.sync_magazine import router as syncMagazine_router
@@ -9,7 +9,7 @@ from app.routes.sync_games import router as syncgames_router
 
 app = FastAPI()
 
-app.include_router(users_router)
+# app.include_router(users_router)
 app.include_router(sync_router)
 app.include_router(synctvshows_router)
 app.include_router(syncMagazine_router)
