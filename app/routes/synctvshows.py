efd936efd82c6dc057cb5c8d4d1a2346817d4936
Suhail-dev1 <suhailansari3695@gmail.com>
2025-04-21 17:19:41 +0530

    Folder does not exist in Pendrive

--- app/routes/synctvshows.py
+++ app/routes/synctvshows.py
@@ -1,16 +1,26 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse
-from app.db import get_db_connection
-import mysql.connector
-import requests
 import os
 import shutil
 from pathlib import Path
-from app.utils.getFileSize import list_files_with_sizes, list_folders_with_sizes
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+import requests
+from app.db import get_db_connection
+from app.utils.getFileSize import list_files_with_sizes
 
 router = APIRouter()
 
 apiEndPointBaseUrl = "https://ifeanalytics-api.aerohub.aero/api/deviceContent/"
+
+def check_folder_existence(folder_path: str):
+    """Check if the folder exists and is a directory."""
+    folder_path = Path(folder_path)
+    print(f"Checking if the folder exists: {folder_path}")
+    if folder_path.exists() and folder_path.is_dir():
+        print(f"Folder exists: {folder_path}")
+        return True
+    else:
+        print(f"Folder does not exist: {folder_path}")
+        return False
 
 @router.get("/syncTvshows")
 def sync_tv_shows():
@@ -36,7 +46,7 @@ def sync_tv_shows():
     try:
         for item in response_data["data"]:
             tvshow_id = item.get("id")
-            
+
             if item.get("is_deleted") == 1:
                 # Delete associated files
                 for path in [item.get("src"), item.get("p_src"), item.get("bd_src")]:
@@ -90,8 +100,60 @@ def sync_tv_shows():
                         type=VALUES(type), attached_id=VALUES(attached_id), episode_num=VALUES(episode_num)
                 """, tv_data)
 
-                db.commit()
-                output.append({"status": 200, "message": "Tvshow synced", "id": tvshow_id})
+                # === Copying logic ===
+                exists = ''
+                copy = ''
+
+                base_path = "/media/suhail/891D-C373/content/tvshows"
+                if check_folder_existence(base_path + item['TMDbId']): # Checking in Pendrive
+                    source_folder = Path(base_path + item['TMDbId'])  # Pendrive path
+                    destination_folder = Path(f"/home/suhail/Python_Project/python_crud_fastapi/public/tvshows")  # Box path
+                    
+                    destination_folder.mkdir(parents=True, exist_ok=True)
+                    final_destination = destination_folder / source_folder.name
+
+                    if check_folder_existence(f"/home/suhail/Python_Project/python_crud_fastapi/public/tvshows{item['TMDbId']}"):
+                        source_common_folder = Path(source_folder / 'common')
+                        destination_common_folder = Path(destination_folder / item['TMDbId'] / 'common')
+
+                        if source_common_folder.exists() and source_common_folder.is_dir() and destination_common_folder.exists() and destination_common_folder.is_dir():
+                            source_files = list_files_with_sizes(source_common_folder)['files']
+                            destination_files = list_files_with_sizes(destination_common_folder)['files']
+                            src_files_set = {(f["name"], f["size_bytes"]) for f in source_files}
+                            des_files_set = {(f["name"], f["size_bytes"]) for f in destination_files}
+                            
+                            for file in source_files:
+                                if (file["name"], file["size_bytes"]) not in des_files_set:
+                                    src_path = os.path.join(source_common_folder, file["name"])
+                                    dest_path = os.path.join(destination_common_folder, file["name"])
+                                    print(f"Copying {file['name']} from source to destination...")
+                                    shutil.copy2(src_path, dest_path)
+                            copy = "Some files copied."
+                        else:
+                            shutil.copytree(source_folder, final_destination, dirs_exist_ok=True)
+                            copy = f"Copied '{source_folder}' to '{final_destination}'"
+                    else:
+                        exists = "Not exists in box."
+                        if source_folder.exists() and source_folder.is_dir():
+                            shutil.copytree(source_folder, final_destination, dirs_exist_ok=True)
+                            copy = f"Copied '{source_folder}' to '{final_destination}'"
+                        else:
+                            copy = "Source folder does not exist or is not a directory."
+                else:
+                    exists = "Folder does not exist in Pendrive."
+
+                output.append({
+                    "tvshow_id": item['id'],
+                    "message": f"{item['title']} has been updated",
+                    "status": "true",
+                    "code": "200",
+                    "is_exists": exists,
+                    "copied": copy
+                })
+                print(f"✅ Upserted movie: {item['title']} (ID: {item['id']})")
+
+        db.commit()
+        output.append({"status": 200, "message": "Tvshow synced and media copied", "id": tvshow_id})
 
     except Exception as db_error:
         db.rollback()
